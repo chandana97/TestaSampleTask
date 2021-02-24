6b8f9bfe08155e44f3c5a572f4f315ba4e8db3b3
chandana97 <32258450+chandana97@users.noreply.github.com>
2021-02-24 19:10:54 +0530

    Create config.tf

---
+++ config.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+resource "aws_key_pair" "mykey"{
+key_name="mykey"
+public_key=file(var.PATH_TO_PUBLIC_KEY)
+}
+resource "aws_instance" "example"{
+ami = "ami-07f5c641c23596eb9"
+instance_type ="t2.micro"
+key_name = "${aws_key_pair.mykey.key_name}"
+connection{
+user = "${var.INSTANCE_USERNAME}"
+private_key =file(var.PATH_TO_PRIVATE_KEY)
+}
+}
+
+variable "PATH_TO_PUBLIC_KEY"{
+ default ="mykey.pub"
+}
+variable "PATH_TO_PRIVATE_KEY"{
+ default ="mykey"
+}
+variable "INSTANCE_USERNAME"{
+ default="linux"
+}
